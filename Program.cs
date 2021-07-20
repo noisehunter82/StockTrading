855023b1ee58d6d30cff9249f3191a813fb6a809
MacOS host <69931086+noisehunter82@users.noreply.github.com>
2021-07-20 19:24:21 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockTrading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input list of prices");
+        }
+    }
+}
